db62ca6e330b756cf2f55c9d298e6f018e9e580c
Juan Mora <raicerk@gmail.com>
2017-08-06 12:53:35 -0400

    Comentarios finales

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	//Declaración de variables
+	nombre: string = "Pro-Gramadores";
+	datos: any;
+	ropa: Array<string> = ["polera", "pantalon"]; //arreglo de datos
 
-  }
+	constructor(public navCtrl: NavController, public http: Http) {
 
+		//consumir una api rest
+		this.http.get("http://mindicador.cl/api")
+			.map(res => res.json())
+			.subscribe(data => {
+				this.datos = JSON.stringify(data.dolar.valor);
+			});
+	}
+
+	//función de onclick
+	saludo() {
+		this.nombre = "Hola a todos";
+	}
+
+	//función para agregar datos a una lista
+	agregar() {
+		this.ropa.push(this.nombre);
+	}
+
+	//función para eliminar datos de la lista
+	eliminar() {
+		this.ropa.pop();
+	}
+
+	//función que retorna un dato
+	retorno() {
+		return "esto";
+	}
 }
